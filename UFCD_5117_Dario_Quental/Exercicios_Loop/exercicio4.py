63108e078ecbbff06e2471798cb0be4294527cf9
RickardoDv <RickardoDv@gmail.com>
2025-04-04 22:26:58 +0100

    update

---
+++ UFCD_5117_Dario_Quental/Exercicios_Loop/exercicio4.py
@@ -0,0 +1,10 @@
+num=int(input("introduza um numero inteiro. "))
+i=0
+for i in range(num):
+    if num%2==0:
+        print ("O numero é primo")
+    else:
+        print ("O numero não é primo")
+        
+        
+        #errado
