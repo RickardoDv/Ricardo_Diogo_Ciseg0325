5ea8b7a84554ab27ad14d1fa11f3c024ea4256f0
RickardoDv <RickardoDv@gmail.com>
2025-04-07 18:56:53 +0100

    update

--- UFCD_5117_Dario_Quental/Exercicios_Loop/exercicio16.py
+++ UFCD_5117_Dario_Quental/Exercicios_Loop/exercicio16.py
@@ -1,2 +1,13 @@
 #Elabore um programa que constitua a média de 30 números pares que sejam introduzidos.
 #Validando a entrada de números inteiros entre 1 e 50.
+count=1
+lista=[]
+while count<=30:
+    num1=int(input("Introduza um numero par entre 1 e 50: "))
+    if num1%2==0:
+        if num1 <=50 and num1 >= 1:
+                lista.append(num1)
+                count+=1
+    else:
+        print("O numero tem que ser par e estar entre 1 e 51:")
+print(lista)
