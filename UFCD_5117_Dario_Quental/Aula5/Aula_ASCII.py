9b4cf0b86bbac800ffccdcb3f481d00f7af027df
RickardoDv <RickardoDv@gmail.com>
2025-04-14 16:07:11 +0100

    a

--- UFCD_5117_Dario_Quental/Aula5/Aula_ASCII.py
+++ UFCD_5117_Dario_Quental/Aula5/Aula_ASCII.py
@@ -11,3 +11,13 @@ while i<= 258:
     time.sleep(0.5)
     i+=1
     
+    
+    
+    import time
+
+i=0
+while i<=258:
+    print(f"Numero por Caracteres {chr(i)}" )
+    print(f"Caracteres por numero {ord(chr(i))}")
+    time.sleep(1)
+    i+=1
