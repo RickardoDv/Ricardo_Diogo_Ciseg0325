0001ae79bfade7ce8b9118fd9ef40e5c7776f35a
RickardoDv <RickardoDv@gmail.com>
2025-05-09 15:35:30 +0100

    cleanup

--- Pratica.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("==== MENU ====")
-    print("1-inserir numeros")
-    print("1-inserir numeros")
-    print("1-inserir numeros")
-    print("1-inserir numeros")
--- UFCD_5117_Dario_Quental/Requests.py
+++ UFCD_5117_Dario_Quental/Requests.py
@@ -1,47 +1,16 @@
 import requests
 
-# URL fornecida
-url = "https://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx"
-params = {
-    "command": "_SelectAllSchedulesDataSetGivenByUserId",
-    "oId": 7199,
-    "idField": "DataValueField",
-    "titleField": "DataTextField",
-    "startDateField": "DataStartField",
-    "endDateField": "DataEndField",
-    "backgroundColorField": "",
-    "textColorField": "textcolor",
-    "eventColorField": "color",
-    "description": "description",
-    "picField": "pic",
-    "urlField": "url",
-    "start": 1747081259,
-    "end": 1747004400,
-    "_": 1746734789633
-}
-
-# Nome a procurar (case insensitive)
-nome_procurado = "rogélio"
+id = int(input("Starting ID: "))
 
 try:
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    data = response.json()
-
-    encontrados = []
-    for item in data:
-        # Verifica todas as strings no dicionário
-        if any(nome_procurado in str(valor).lower() for valor in item.values()):
-            encontrados.append(item)
-
-    if encontrados:
-        print(f"Foram encontrados {len(encontrados)} resultados com o nome '{nome_procurado}':\n")
-        for e in encontrados:
-            print(e)
-    else:
-        print(f"Nenhum resultado encontrado com o nome '{nome_procurado}'.")
-
-except requests.exceptions.RequestException as e:
-    print("Erro ao acessar a URL:", e)
-except ValueError:
-    print("Resposta não é um JSON válido.")
+    while True:
+        response = requests.get(f"https://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx?command=_SelectAllSchedulesDataSetGivenByUserId&oId={id}&idField=DataValueField&titleField=DataTextField&startDateField=DataStartField&endDateField=DataEndField&backgroundColorField=&textColorField=textcolor&eventColorField=color&description=description&picField=pic&urlField=url&start=1746399600&end=1747004400&_=1746736138904")
+        if "Sessão como Formador" in response.text and "Rogélio" in response.text:
+            print(f"Apanhado! {id}")
+            id += 1
+            break
+        else:
+            id += 1
+            print(id)
+except:
+    print(id)
