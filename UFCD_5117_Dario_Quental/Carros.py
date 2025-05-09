57d90aca65dab7a255d67168f051a558ddf38f8b
RickardoDv <RickardoDv@gmail.com>
2025-05-09 19:08:22 +0100

    update

--- UFCD_5117_Dario_Quental/Carros.py
+++ UFCD_5117_Dario_Quental/Carros.py
@@ -1,9 +1,29 @@
 class Carro:
-    def __init__(self,marca,modelo,ano,vel):
+    def __init__(self,marca,modelo,ano):
         self.marca=marca
         self.modelo=modelo
         self.ano=ano
-        self.vel=vel
+        self.vel=0
+        self.ligado=False
+        self.luzes=False
+    
+    def getmarca (self):
+        return self.marca
+    
+    def getmodelo (self):
+        return self.modelo 
+      
+    def getano (self):
+        return self.ano
+    
+    def getvelocidade (self):
+        return self.vel
+    
+    def getligado (self):
+        return self.ligado
+    
+    def getluzes (self):
+        return self.luzes
     
     def Acelera(self,nivel):
         if nivel==1:
@@ -21,17 +41,17 @@ class Carro:
         if nivel==3:
             self.vel-=20
             
-Carro1=Carro("BMW","M3","2020",0)
-print("Velocidade", Carro1.vel)
-Carro1.Acelera(1)
-print("Velocidade", Carro1.vel)
+Carro1=Carro("BMW","M3","2020")
+print("Velocidade", Carro1.getvelocidade())
+Carro1.Acelera(int(input("qual a Velocidade de 1 a 3")))
+print("Velocidade", Carro1.getvelocidade())
 Carro1.Acelera(2)
-print("Velocidade", Carro1.vel)
+print("Velocidade", Carro1.getvelocidade())
 Carro1.Travar(2)
-print("Velocidade", Carro1.vel)
+print("Velocidade", Carro1.getvelocidade())
 Carro1.Acelera(3)
-print("Velocidade", Carro1.vel)
+print("Velocidade", Carro1.getvelocidade())
 Carro1.Travar(3)
-print("Velocidade", Carro1.vel)
+print("Velocidade", Carro1.getvelocidade())
 Carro1.Travar(1)
-print("Velocidade", Carro1.vel)
+print("Velocidade", Carro1.getvelocidade())
